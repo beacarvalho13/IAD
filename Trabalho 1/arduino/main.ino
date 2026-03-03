72af569009eaf24a7e0032a8a24d56d31efc2e36
beacarvalho13 <beacarvalhogoncalves@gmail.com>
2026-03-03 11:00:40 +0000

    --all

--- Trabalho 1/arduino/main.ino
+++ Trabalho 1/arduino/main.ino
@@ -6,6 +6,12 @@ float tesla;
 void setup() {
   Serial.begin(9600);
   pinMode(hallPin, INPUT);
+
+  if (estado == HIGH) {
+      Serial.println("Connection established");
+    } else {
+      Serial.println("No connection");
+    }
 }
 
 void loop() {
@@ -18,22 +24,37 @@ void loop() {
     if (command == "MEASURE"){
 
       val = analogRead(hallPin);
-      Serial.println(val);
 
       voltage = val * (5.0 / 1023.0);
-      Serial.println(voltage);
 
       tesla = (voltage - 2.5)/ 0.018;
+
+      Serial.print("Raw: ");
+      Serial.print(val);
+      Serial.print(" | Volts: ");
+      Serial.print(voltage);
+      Serial.print(" | Tesla: ");
       Serial.println(tesla);
 
+      if (val == 0 || val == 1023) {
+        Serial.print("WARNING: Pin reading extreme value (");
+        Serial.print(val);
+        Serial.println("). Check sensor connection.");
+
+    }
+
+    if (command.length() == 0) {
+      Serial.println("ERROR: Empty command received.");
     }
 
     else {
 
       Serial.print("ERROR");
+      Serial.println(command);
 
     }
   }
 
   delay(5000);
+  }
 }
