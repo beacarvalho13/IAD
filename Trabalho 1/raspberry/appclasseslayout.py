40f89cf4e45012e694c8e1b274017031194bae53
beacarvalho13 <beacarvalhogoncalves@gmail.com>
2026-03-10 15:42:39 +0000

    --all

--- Trabalho 1/raspberry/appclasseslayout.py
+++ Trabalho 1/raspberry/appclasseslayout.py
@@ -272,7 +272,7 @@ class MyWindow(QMainWindow):
         self.scatter.setData(spots)
 
         # Auto scale graph
-        self.plot_widget.enableAutoRange(axis='y')
+        #self.plot_widget.enableAutoRange(axis='y')
 
     '''def mouse_moved(self, event):
         pos = event[0]  # get mouse position
