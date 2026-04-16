6b3bec85df46d0ac0f016f7c0d1a88d629aa4fc0
beacarvalho13 <beacarvalhogoncalves@gmail.com>
2026-04-16 14:40:24 +0100

    --all

--- FSR/FSR.ino
+++ FSR/FSR.ino
@@ -2,30 +2,39 @@
 #include <BLEUtils.h>
 #include <BLEServer.h>
 
-#define DEVICE_NAME "TALKY_BUDDY"
+#define DEVICE_NAME "TALKY_BUDDY!!!"
 
 BLEAdvertising *pAdvertising;
 const int fsrPin = A2;
 int threshold = 1000;
 bool isPressing = false;
 unsigned long pressStartTime = 0;
+uint8_t globalCounter = 0; // Coloca isto no topo do código
 
 void sendSignal(uint8_t s) {
-  String mData = "";
-  
-  mData += (char)0xFF;   //identifier
-  mData += (char)s;   //signal
+  globalCounter++; // ISTO É VITAL: Muda o ID a cada envio
+  if (globalCounter > 250) globalCounter = 1;
+
+  uint8_t msd[4]; 
+  msd[0] = 0xFF; // Company ID Byte 1
+  msd[1] = 0xFF; // Company ID Byte 2
+  msd[2] = globalCounter; 
+  msd[3] = s;
 
   BLEAdvertisementData advData;
-  advData.setName(DEVICE_NAME);
-  advData.setManufacturerData(mData);
+  advData.setFlags(0x06); 
+  advData.setManufacturerData(String((char*)msd, 4));
 
-  pAdvertising->stop(); 
+  BLEAdvertisementData scanResponse;
+  scanResponse.setName(DEVICE_NAME);
+
+  pAdvertising->stop();
   pAdvertising->setAdvertisementData(advData);
-  pAdvertising->start(); 
+  pAdvertising->setScanResponseData(scanResponse);
+  pAdvertising->start();
   
-  Serial.print("Signal: ");
-  Serial.println(s);
+  Serial.print("Sinal: "); Serial.print(s);
+  Serial.print(" | ID: "); Serial.println(globalCounter);
 }
 
 void setup() {
@@ -58,11 +67,12 @@ void loop() {
     unsigned long duration = currentTime - pressStartTime;
     uint8_t signal = 0;
 
-    if (duration >= 50 && duration < 1000) signal = 1;   //dot
-    else if (duration >= 1000) signal = 2;   //dash
+    if (duration >= 200 && duration < 1000) signal = 1;   //dot
+    else if (duration >= 1500) signal = 2;   //dash
 
     if (signal > 0) {
       sendSignal(signal);
+      delay(100);
       lastSignalTime = millis(); 
       estadoTimeout = 1;   //waiting for end of character
     }
