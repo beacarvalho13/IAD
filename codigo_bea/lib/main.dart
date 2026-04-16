3a2ce38e6790685cdbae5d7105fbd8814974d609
kodakk33 <rmcraimundo@gmail.com>
2026-04-17 00:38:06 +0100

    --all

--- codigo_bea/lib/main.dart
+++ codigo_bea/lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:meu_projeto/models/app_mode.dart';
 import 'package:meu_projeto/screens/home_screen.dart' hide CommunicationMode;
-//import 'screens/home_screen.dart';
+
+// Main entry point of the app, which initializes the Flutter bindings and runs the main widget
 
 void main() {
-  // Garante que os bindings do Flutter estão inicializados antes de começar
+  // Ensures that Flutter bindings are initialized before running the app, then runs the main widget
   WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key});// Main widget of the app, which listens to the communication mode and updates the theme accordingly
 
   @override
   Widget build(BuildContext context) {
     return ValueListenableBuilder<CommunicationMode>(
       valueListenable: appMode,
       builder: (context, mode, _) {
-        final isMorse = mode == CommunicationMode.morse;
+        final isMorse = mode == CommunicationMode.morse;// Determines if the current mode is Morse or Words to set the theme color
 
         return MaterialApp(
-          title: 'BLE Sensor App',
+          title: 'BLE Sensor App',// Title of the app
           debugShowCheckedModeBanner: false,
 
           theme: ThemeData(
